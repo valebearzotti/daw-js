2b93e4e032d9131f9c7086159975e82dc219927b
valebearzotti <valebearzotti1@gmail.com>
2024-06-08 19:59:40 -0300

    variables and operators

--- exercises/variables_and_operators.js
+++ exercises/variables_and_operators.js
@@ -1,3 +1,27 @@
 /*
 Variables and Operators
 */
+
+// Ejercicio 1
+
+console.log("Ejercicio 1");
+var num1 = 5;
+var num2 = 10;
+var suma = num1 + num2;
+console.log("Suma: " + suma);
+
+// Ejercicio 2
+
+console.log("Ejercicio 2");
+var str1 = "Hello";
+var str2 = " World!";
+var concatenado = str1 + str2;
+console.log("Concatenado: " + concatenado);
+
+// Ejercicio 3
+
+console.log("Ejercicio 3");
+var str3 = "Hello";
+var str4 = "World!";
+var largoSuma = str3.length + str4.length;
+console.log("Largo de la suma: " + largoSuma);
